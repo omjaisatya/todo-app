ff1cbca3a537bf66d241aad13b25aa7bd3d69796
omjaisatya <satyaprakash451256@gmail.com>
2024-06-18 14:10:23 +0530

    add ejs file

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = 3000;
 
 // init app
 const app = express();
 
+const connectionUrl = "mongodb://localhost:27017/todoDb";
+
+mongoose
+  .connect(connectionUrl)
+  .then(() => console.log("Database connection succesfully"))
+  .catch((error) => console.log(error.message));
+
 // view engine
 app.set("view engine", "ejs");
+
+app.get("/", (req, res, next) => {
+  try {
+    res.render("index");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // listen
 app.listen(PORT, () => {
