6961ee1b6a03975518aaeb138156f786d78b4d9e
omjaisatya <satyaprakash451256@gmail.com>
2024-06-19 15:05:55 +0530

    add some file with extra features

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { timeStamp } = require("console");
 const express = require("express");
 const mongoose = require("mongoose");
+const { type } = require("os");
+const path = require("path");
 
 const PORT = 3000;
 
@@ -13,12 +16,47 @@ mongoose
   .then(() => console.log("Database connection succesfully"))
   .catch((error) => console.log(error.message));
 
+const todoSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    desc: String,
+  },
+  { timestamp: true }
+);
+
+const Todo = mongoose.model("todo", todoSchema);
+
 // view engine
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res, next) => {
   try {
-    res.render("index");
+    res.render("index", { title: "List todo" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/add-todo", (req, res, next) => {
+  try {
+    res.render("newTodo", { title: "New todo" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/update-todo", (req, res, next) => {
+  try {
+    res.render("updateTodo", { title: "Update Todo" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/delete-todo", (req, res, next) => {
+  try {
+    res.render("deleteTodo", { title: "Delete Todo" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
